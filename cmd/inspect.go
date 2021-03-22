df2e216f10a61ea88d208ca2f30b338d94a43430
kyoshiro.maruo <kyoshiro-maruo@m3.com>
2021-03-22 20:13:53 +0900

    fix inspect

--- cmd/inspect.go
+++ cmd/inspect.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/spf13/cobra"
 	"golang.org/x/xerrors"
 
@@ -16,14 +21,29 @@ func newInspectCmd() *cobra.Command {
 		Use:   "inspect https://redash.yourdomain.com/queries/12345",
 		Short: "display required group for query or datasource or dashboard",
 		Args: func(cmd *cobra.Command, args []string) error {
-			if len(args) != 1 {
-				return xerrors.New("query id is required")
+			if len(args) > 1 {
+				return xerrors.New("only 1 url must be specified")
 			}
-
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return impl.InspectCmd(globalClient, args[0])
+			var arg string
+			if len(args) == 0 {
+				fmt.Printf("Type target URL (e.g. https://redash.yourdomain.com/queries/12345): ")
+
+				reader := bufio.NewReader(os.Stdin)
+				res, err := reader.ReadString('\n')
+
+				if err != nil {
+					return err
+				}
+
+				arg = strings.TrimSpace(res)
+			} else {
+				arg = args[0]
+			}
+
+			return impl.InspectCmd(globalClient, arg)
 		},
 	}
 
